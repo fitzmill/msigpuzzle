bfc1f34bdad3ea84d5356ef8b61dd96e56b3e942
Sean Fitzgerald <sfitzmill@gmail.com>
2019-06-24 23:56:17 -0700

    Updates

---
+++ precache-manifest.290a1f0e8ba97f802115750f585ee2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21afc2f33aaaceb45a3e248f0b79a83f",
+    "url": "/msigpuzzle/index.html"
+  },
+  {
+    "revision": "d9e1abcb8801d89e4b53",
+    "url": "/msigpuzzle/static/css/2.f5b2e8f7.chunk.css"
+  },
+  {
+    "revision": "a42fb75dfa0ae4814049",
+    "url": "/msigpuzzle/static/css/main.ce3220d5.chunk.css"
+  },
+  {
+    "revision": "d9e1abcb8801d89e4b53",
+    "url": "/msigpuzzle/static/js/2.0dffaea0.chunk.js"
+  },
+  {
+    "revision": "a42fb75dfa0ae4814049",
+    "url": "/msigpuzzle/static/js/main.f6da34e0.chunk.js"
+  },
+  {
+    "revision": "d2e8398f89777b40fcb0",
+    "url": "/msigpuzzle/static/js/runtime~main.691ce955.js"
+  }
+]);
