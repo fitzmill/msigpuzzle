06a10e673229bebf9cc0b85463f5fcc824137cc6
Sean Fitzgerald <sfitzmill@gmail.com>
2019-06-24 23:49:46 -0700

    Fixed weird styling issues

--- src/App.js
+++ src/App.js
@@ -44,40 +44,42 @@ export default class App extends React.Component {
     return (
       <div className="App uk-light">
         <header className="App-header">
-          <h1>MSIG Puzzle</h1>
+          <h1 className="uk-margin-top">MSIG Puzzle</h1>
         </header>
         <div className="uk-card card uk-align-center">
-          <p>Hello! This is the page to submit answers to the puzzle we submitted as a part of our application to the Microsoft Intern Game.</p>
-          <p>If you don't know what this page is, you probably shouldn't be here.</p>
+          <div className="uk-margin-left uk-margin-right">
+            <p>Hello! This is the page to submit answers to the puzzle we submitted as a part of our application to the Microsoft Intern Game.</p>
+            <p>If you don't know what this page is, you probably shouldn't be here.</p>
 
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              className={'uk-input ' + (inputError ? 'input-error ' : '') + (solved ? 'input-success' : '')}
-              placeholder="Submit an answer here"
-              value={input}
-              onChange={this.onChange} />
-    
-            <button
-              type="submit"
-              className="uk-button uk-button-default uk-margin"
-              >Submit</button>
-          </form>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                className={'uk-input ' + (inputError ? 'input-error ' : '') + (solved ? 'input-success' : '')}
+                placeholder="Submit an answer here"
+                value={input}
+                onChange={this.onChange} />
+      
+              <button
+                type="submit"
+                className="uk-button uk-button-default uk-margin"
+                >Submit</button>
+            </form>
 
-          {solved &&
-            <div className="uk-card uk-text-success">
-              <p>Good job! Now that you've found the spell to open doors, we're hoping that you'll open our door to the intern game!</p>
-            </div>
-          }
+            {solved &&
+              <div className="uk-card uk-text-success">
+                <p>Good job! Now that you've found the spell to open doors, we're hoping that you'll open our door to the intern game!</p>
+              </div>
+            }
 
-          {messages.map((element, index) => {
-              return (
-                <div className="uk-card uk-text-warning" key={index}>
-                  <p>{element}</p>
-                </div>
-              );
-            })
-          }
+            {messages.map((element, index) => {
+                return (
+                  <div className="uk-card uk-text-warning" key={index}>
+                    <p>{element}</p>
+                  </div>
+                );
+              })
+            }
+          </div>
         </div>
       </div>
     );
