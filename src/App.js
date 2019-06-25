82fdca935083278758e007cb278a9050190cf42f
Sean Fitzgerald <sfitzmill@gmail.com>
2019-06-24 23:28:44 -0700

    finished demo, waiting for actual solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      input: '',
+      messages: [],
+      solved: false,
+      inputError: false
+    }
 
-export default App;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let { input, messages, solved, inputError } = this.state;
+
+    input = input.toLocaleLowerCase();
+
+    if (input === 'sean') {
+      messages.unshift('Close');
+      inputError = false;
+    } else if (input === 'alli') {
+      solved = true;
+      inputError = false;
+    } else {
+      inputError = true;
+    }
+
+    this.setState({ messages, solved, inputError });
+  }
+
+  onChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
+  render() {
+    const { input, messages, solved, inputError } = this.state;
+    return (
+      <div className="App uk-light">
+        <header className="App-header">
+          <h1>MSIG Puzzle</h1>
+        </header>
+        <div className="uk-card card uk-align-center">
+          <p>Hello! This is the page to submit answers to the puzzle we submitted as a part of our application to the Microsoft Intern Game.</p>
+          <p>If you don't know what this page is, you probably shouldn't be here.</p>
+
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              className={'uk-input ' + (inputError ? 'input-error ' : '') + (solved ? 'input-success' : '')}
+              placeholder="Submit an answer here"
+              value={input}
+              onChange={this.onChange} />
+    
+            <button
+              type="submit"
+              className="uk-button uk-button-default uk-margin"
+              >Submit</button>
+          </form>
+
+          {solved &&
+            <div className="uk-card uk-text-success">
+              <p>Good job! Now that you've found the spell to open doors, we're hoping that you'll open our door to the intern game!</p>
+            </div>
+          }
+
+          {messages.map((element, index) => {
+              return (
+                <div className="uk-card uk-text-warning" key={index}>
+                  <p>{element}</p>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
